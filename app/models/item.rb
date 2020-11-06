a95d2dcfa97ea4a43f327788ae168d2f1251ebbe
uki62922 <uki62922@yahoo.co.jp>
2020-11-06 18:31:50 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,7 @@ class Item < ApplicationRecord
   validates :postage_id , presence: true
   validates :area_id, presence: true
   validates :days_deliver_id, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, format: {with: /\A[0-9]+\z/,greater_than_or_equal_to: 300,less_than_or_equal_to: 99999999}
   validates :image, presence: true
 
 end
