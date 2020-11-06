ad40aa7b6cbb99930f175f7de6f4cc1ff7318625
uki62922 <uki62922@yahoo.co.jp>
2020-11-06 15:45:10 +0900

    バリデーションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,10 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+
+  private
+   def item_params
+    params.require(:item).permit(:goods, :item_text, :category_id, :condition_id, :postage_id, :area_id, :days_deliver_id, :price, :image).merge(user_id: current_user.id)
+   end
+   
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,17 @@
 class Item < ApplicationRecord
-belongs_to :user
-has_one :order
-has_one_attached :image
+
+  belongs_to :user
+  has_one :order
+  has_one_attached :image
+
+  validates :goods, presence: true
+  validates :item_text, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :postage_id , presence: true
+  validates :area_id, presence: true
+  validates :days_deliver_id, presence: true
+  validates :price, presence: true
+  validates :image, presence: true
 
 end
