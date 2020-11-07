4d0bbed82f5d20119c064b689909a0c98d0530a2
uki62922 <uki62922@yahoo.co.jp>
2020-11-07 19:29:36 +0900

    商品出品機能の修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -4,7 +4,6 @@ window.addEventListener('load', () => {
     const inputValue = priceInput.value;
     const addTaxDom = document.getElementById("add-tax-price");
     fee = (Math.floor(inputValue * 0.1 ))
-    console.log(fee)
     addTaxDom.innerHTML = fee
     const addGainDom = document.getElementById("profit");
     addGainDom.innerHTML = (inputValue - fee )
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,25 +4,24 @@ class Item < ApplicationRecord
   has_one :order
   has_one_attached :image
 
-  validates :goods, presence: true
-  validates :item_text, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :postage_id , presence: true
-  validates :area_id, presence: true
-  validates :days_deliver_id, presence: true
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999}, format: {with: /\A[0-9]+\z/}
-  validates :image, presence: true
-
+  with_options presence: true do
+    validates :goods
+    validates :item_text
+    validates :price, numericality: { greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999}, format: {with: /\A[0-9]+\z/}
+    validates :image
+  end
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :area
   belongs_to :condition
   belongs_to :postage
   belongs_to :days_deliver
-  validates :category_id, numericality: { other_than: 1 } 
-  validates :area_id, numericality: { other_than: 1 }
-  validates :condition_id, numericality: { other_than: 1 } 
-  validates :postage_id, numericality: { other_than: 1 }
-  validates :days_deliver_id, numericality: { other_than: 1 }
+
+  with_options numericality: { other_than: 1 }  do
+    validates :category_id
+    validates :area_id
+    validates :condition_id 
+    validates :postage_id
+    validates :days_deliver_id
+  end
 end
