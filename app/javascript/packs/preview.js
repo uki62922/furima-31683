43afc47b5e1f7e0e36551f03483b684f0f3fcb74
uki62922 <uki62922@yahoo.co.jp>
2020-11-14 15:19:41 +0900

    プレビュー機能の実装

---
+++ app/javascript/packs/preview.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function(){
+  const ImageList = document.getElementById('image-list');
+  document.getElementById('item-image').addEventListener('change', function(e){
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+
+    const imageElement = document.createElement('div');
+    const blobImage = document.createElement('img');
+    blobImage.setAttribute('src', blob);
+    
+    
+    blobImage.classList.add('preview-image');
+    imageElement.appendChild(blobImage);
+    ImageList.appendChild(imageElement);
+  });
+});
